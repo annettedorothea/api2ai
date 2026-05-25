ca6b2bbea10ddc232e0b15c413db935a179e61b5
annettedorothea <github@anfelisa.de>
2026-05-25 19:19:19 +0200

    add test coverage

--- packages/cli/vitest.config.ts
+++ packages/cli/vitest.config.ts
@@ -3,6 +3,12 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
     test: {
         include: ['test/**/*.test.ts'],
-        exclude: ['out/**', 'node_modules/**']
+        exclude: ['out/**', 'node_modules/**'],
+        coverage: {
+            provider: 'v8',
+            reporter: ['text', 'html', 'lcov'],
+            include: ['src/**/*.ts'],
+            exclude: ['out/**']
+        }
     }
 });
